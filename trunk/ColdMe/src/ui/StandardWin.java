3faf7b30da3ae221294e42d454bd30fe5d34a858
anthony.depale@gmail.com <anthony.depale@gmail.com@2ccba944-7e53-12bc-b049-c6dbf296a0f6>
2012-01-25 20:41:43 +0000

    Logo added

--- trunk/ColdMe/src/ui/StandardWin.java
+++ trunk/ColdMe/src/ui/StandardWin.java
@@ -3,21 +3,17 @@ package ui;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 /**
  * Summary description for StandardWin
  *
  */
 public class StandardWin extends JFrame
 {
-	private JLabel jLabel2;
-	private JTextField jTextField1;
-	private JTextField jTextField2;
-	private JButton jButton1;
-	private JPanel contentPane;
 	private JTextField textField;
-	// End of variables declaration
-
-
+	private JTextField textField_1;
+	private JTextField textField_2;
+	
 	public StandardWin()
 	{
 		super();
@@ -39,53 +35,15 @@ public class StandardWin extends JFrame
 	 */
 	private void initializeComponent()
 	{
-		jLabel2 = new JLabel();
-		jTextField1 = new JTextField();
-		jTextField2 = new JTextField();
-		jButton1 = new JButton();
-		contentPane = (JPanel)this.getContentPane();
-		//
-		// jLabel2
-		//
-		jLabel2.setText("jLabel2");
-		//
-		// jTextField1
-		//
-		jTextField1.setText("IP");
-		jTextField1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)
-			{
-				jTextField1_actionPerformed(e);
-			}
-
-		});
-		//
-		// jTextField2
-		//
-		jTextField2.setText("Threads");
-		jTextField2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)
-			{
-				jTextField2_actionPerformed(e);
-			}
-
-		});
-		//
-		// jButton1
-		//
-		jButton1.setText("jButton1");
-		jButton1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)
-			{
-				jButton1_actionPerformed(e);
-			}
-
-		});
-		addComponent(contentPane, jLabel2, 16,38,60,18);
-		addComponent(contentPane, jTextField1, 84,35,221,22);
-		addComponent(contentPane, jTextField2, 84,64,80,22);
-		addComponent(contentPane, jButton1, 306,170,83,28);
+		
 		getContentPane().setLayout(null);
+		
+		
+		JLabel image = new JLabel();
+		image.setIcon(new ImageIcon(StandardWin.class.getResource("/ui/logo-mines.jpg")));
+		image.setBounds(10, 27, 303, 285);
+		getContentPane().add(image);
+		
 		
 		JComboBox protocol = new JComboBox();
 		protocol.setFont(new Font("Tahoma", Font.PLAIN, 15));
@@ -95,7 +53,10 @@ public class StandardWin extends JFrame
 		protocol.setSelectedIndex(0);
 		getContentPane().add(protocol);
 		
+		
+		
 		JButton btnZentaaar = new JButton("ZENTARISATION");
+		btnZentaaar.setBackground(Color.RED);
 		btnZentaaar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
@@ -118,12 +79,7 @@ public class StandardWin extends JFrame
 		lblPort.setBounds(484, 368, 37, 22);
 		getContentPane().add(lblPort);
 		
-		textField = new JTextField();
-		textField.setText("80");
-		textField.setFont(new Font("Tahoma", Font.BOLD, 14));
-		textField.setBounds(519, 369, 86, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+	
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 665, 21);
@@ -137,43 +93,66 @@ public class StandardWin extends JFrame
 		
 		JMenuItem mntmAbout = new JMenuItem("About");
 		mnNewMenu_1.add(mntmAbout);
+		
+		Component verticalStrut = Box.createVerticalStrut(20);
+		verticalStrut.setBounds(318, 32, 12, 440);
+		getContentPane().add(verticalStrut);
+		
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		horizontalStrut.setBounds(10, 312, 303, 12);
+		getContentPane().add(horizontalStrut);
+		
+				
+		JLabel lblThreads = new JLabel("Threads");
+		lblThreads.setForeground(Color.DARK_GRAY);
+		lblThreads.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblThreads.setBackground(Color.PINK);
+		lblThreads.setBounds(384, 335, 68, 22);
+		getContentPane().add(lblThreads);
+		
+				
+		JLabel lblFrequency = new JLabel("Frequency");
+		lblFrequency.setForeground(Color.DARK_GRAY);
+		lblFrequency.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblFrequency.setBackground(Color.PINK);
+		lblFrequency.setBounds(369, 302, 83, 22);
+		getContentPane().add(lblFrequency);
+		
+		textField = new JTextField();
+		textField.setText("50\r\n");
+		textField.setFont(new Font("Tahoma", Font.BOLD, 14));
+		textField.setBounds(449, 305, 86, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("50\r\n");
+		textField_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		textField_1.setColumns(10);
+		textField_1.setBounds(449, 337, 86, 20);
+		getContentPane().add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setText("80");
+		textField_2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		textField_2.setColumns(10);
+		textField_2.setBounds(524, 368, 86, 20);
+		getContentPane().add(textField_2);
+		
 		//
 		// StandardWin
 		//
-		this.setTitle("LOIC - Ecole des Mines de Douai - ISIC 2012");
+		this.setTitle("LOIC - ENSM Douai - ISIC 2012");
 		this.setLocation(new Point(0, 0));
 		this.setSize(new Dimension(661, 521));
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
-	/** Add Component Without a Layout Manager (Absolute Positioning) */
-	private void addComponent(Container container,Component c,int x,int y,int width,int height)
-	{
-	}
+	
 
-	//
-	// TODO: Add any appropriate code in the following Event Handling Methods
-	//
-	private void jTextField1_actionPerformed(ActionEvent e)
-	{
-		System.out.println("\njTextField1_actionPerformed(ActionEvent e) called.");
-		// TODO: Add any handling code here
+	
 
-	}
-
-	private void jTextField2_actionPerformed(ActionEvent e)
-	{
-		System.out.println("\njTextField2_actionPerformed(ActionEvent e) called.");
-		// TODO: Add any handling code here
-
-	}
-
-	private void jButton1_actionPerformed(ActionEvent e)
-	{
-		System.out.println("\njButton1_actionPerformed(ActionEvent e) called.");
-		// TODO: Add any handling code here
-
-	}
+	
 
 	//
 	// TODO: Add any method code to meet your needs in the following area
