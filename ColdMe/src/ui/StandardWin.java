3abeef03256ffcbd5d2d840d2ba48bbc95bdac36
anthony.depale <anthony.depale@gmail.com>
2012-01-25 13:13:20 +0000

    Basic Window example using JFrame Builder

--- ColdMe/src/ui/StandardWin.java
+++ ColdMe/src/ui/StandardWin.java
@@ -1,13 +1,199 @@
-package ui;
+/****************************************************************/
+/*                      StandardWin	                            */
+/*                                                              */
+/****************************************************************/
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+/**
+ * Summary description for StandardWin
+ *
+ */
+public class StandardWin extends JFrame
+{
+	// Variables declaration
+	private JLabel jLabel1;
+	private JLabel jLabel2;
+	private JTextField jTextField1;
+	private JTextField jTextField2;
+	private JButton jButton1;
+	private JPanel contentPane;
+	// End of variables declaration
 
-public class StandardWin {
+
+	public StandardWin()
+	{
+		super();
+		initializeComponent();
+		//
+		// TODO: Add any constructor code after initializeComponent call
+		//
+
+		this.setVisible(true);
+	}
 
 	/**
-	 * @param args
+	 * This method is called from within the constructor to initialize the form.
+	 * WARNING: Do NOT modify this code. The content of this method is always regenerated
+	 * by the Windows Form Designer. Otherwise, retrieving design might not work properly.
+	 * Tip: If you must revise this method, please backup this GUI file for JFrameBuilder
+	 * to retrieve your design properly in future, before revising this method.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private void initializeComponent()
+	{
+		jLabel1 = new JLabel();
+		jLabel2 = new JLabel();
+		jTextField1 = new JTextField();
+		jTextField2 = new JTextField();
+		jButton1 = new JButton();
+		contentPane = (JPanel)this.getContentPane();
+
+		//
+		// jLabel1
+		//
+		jLabel1.setText("jLabel1");
+		//
+		// jLabel2
+		//
+		jLabel2.setText("jLabel2");
+		//
+		// jTextField1
+		//
+		jTextField1.setText("IP");
+		jTextField1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				jTextField1_actionPerformed(e);
+			}
+
+		});
+		//
+		// jTextField2
+		//
+		jTextField2.setText("Threads");
+		jTextField2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				jTextField2_actionPerformed(e);
+			}
+
+		});
+		//
+		// jButton1
+		//
+		jButton1.setText("jButton1");
+		jButton1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				jButton1_actionPerformed(e);
+			}
+
+		});
+		//
+		// contentPane
+		//
+		contentPane.setLayout(null);
+		addComponent(contentPane, jLabel1, 18,67,60,18);
+		addComponent(contentPane, jLabel2, 16,38,60,18);
+		addComponent(contentPane, jTextField1, 84,35,221,22);
+		addComponent(contentPane, jTextField2, 84,64,80,22);
+		addComponent(contentPane, jButton1, 306,170,83,28);
+		//
+		// StandardWin
+		//
+		this.setTitle("StandardWin - extends JFrame");
+		this.setLocation(new Point(0, 0));
+		this.setSize(new Dimension(571, 446));
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+
+	/** Add Component Without a Layout Manager (Absolute Positioning) */
+	private void addComponent(Container container,Component c,int x,int y,int width,int height)
+	{
+		c.setBounds(x,y,width,height);
+		container.add(c);
+	}
+
+	//
+	// TODO: Add any appropriate code in the following Event Handling Methods
+	//
+	private void jTextField1_actionPerformed(ActionEvent e)
+	{
+		System.out.println("\njTextField1_actionPerformed(ActionEvent e) called.");
+		// TODO: Add any handling code here
 
 	}
+
+	private void jTextField2_actionPerformed(ActionEvent e)
+	{
+		System.out.println("\njTextField2_actionPerformed(ActionEvent e) called.");
+		// TODO: Add any handling code here
+
+	}
+
+	private void jButton1_actionPerformed(ActionEvent e)
+	{
+		System.out.println("\njButton1_actionPerformed(ActionEvent e) called.");
+		// TODO: Add any handling code here
+
+	}
+
+	//
+	// TODO: Add any method code to meet your needs in the following area
+	//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+//============================= Testing ================================//
+//=                                                                    =//
+//= The following main method is just for testing this class you built.=//
+//= After testing,you may simply delete it.                            =//
+//======================================================================//
+	public static void main(String[] args)
+	{
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JDialog.setDefaultLookAndFeelDecorated(true);
+		try
+		{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Failed loading L&F: ");
+			System.out.println(ex);
+		}
+		new StandardWin();
+	}
+//= End of Testing =
+
 
 }
